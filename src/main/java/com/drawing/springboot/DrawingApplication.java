ccc8e8c5b23ce4e0a16ea683b7389b5e3cbc0465
KWJ <kang65516311@gmail.com>
2026-01-23 09:15:05 +0900

    setting1

---
+++ src/main/java/com/drawing/springboot/DrawingApplication.java
@@ -0,0 +1,13 @@
+package com.drawing.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawingApplication.class, args);
+	}
+
+}
